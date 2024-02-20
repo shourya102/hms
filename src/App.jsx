2703d09eff39f6ecb5cfaed8651769a12f52094f
shourya102 <shourya.sahu456@@gmail.com>
2024-02-20 13:20:09 +0530

    init1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import {BrowserRouter, Routes} from "react-router-dom";
+
 const App = () => {
     return (
         <div className="App">
-            
+            <BrowserRouter>
+                <Routes>
+
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
